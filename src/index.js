307e1a51d75e4279ddadbc87df3eeb8fc4ac4879
FLORIS BUYSE <floris.buyse@student.hogent.be>
2022-10-12 12:19:26 +0200

    transaction, mockdata en services toegevoed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const {
 	getLogger
 } = require('./core/logging')
 const config = require('config');
+const bodyParser = require('koa-bodyparser');
+const Router = require('@koa/router');
+const transactionService = require('./service/transaction');
 
 const NODE_ENV = config.get('env');
 const LOG_LEVEL = config.get('log.level');
@@ -15,10 +18,24 @@ console.log({
 });
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx) => {
-	ctx.body = 'Hello World';
+app.use(bodyParser());
+
+router.get('/api/transactions', async ctx => {
+	getLogger().info(JSON.stringify(ctx.request));
+	ctx.body = transactionService.getAll();
 });
+
+router.post('api/transactions', async ctx => {
+	const newTransaction = transactionService.create({
+		...ctx.request.body,
+		date: new Date(ctx.request.body.date)
+	});
+	ctx.body = newTransaction;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(9000, () => {
 	getLogger().info('Server is listening on http://localhost:9000', {
